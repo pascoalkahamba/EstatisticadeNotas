d0a6f8ce2e5486bd00c21194859f274618162d34
pascoalkahamba <pascoalkahamba25@gmail.com>
2022-07-11 00:25:57 +0100

    tornando o projeto funcional

--- script.js
+++ script.js
@@ -1,61 +1,82 @@
-let solucao = document.querySelector('.solucao')
-let butao = document.querySelector('#butao')
-let formEntrada = document.querySelector('.form1')
-let formRegistar = document.querySelector('#form2')
+const $ = document.querySelector.bind(document)
+let solucao = $('.solucao')
+let butao = $('#butao')
+let formEntrada = $('.form1')
+let formRegistar = $('#form2')
 let butaoRegistar = document.getElementById('butaoRegistar')
-let divPrincipal = document.querySelector('.problema')
-formRegistar.style.display = 'none'
-butaoRegistar.style.display = 'none'
+let divPrincipal = $('.problema')
+let mostraDisciplina = document.getElementsByTagName('h1')[0]
+let nomeDisc = $('#disc-nome')
+let notaDisc = $('#disc-nota')
+let disciplinas = $('#txt-numero')
+let paragrafo = $('.paragrafo')
+    // const disciplina = {
+    //     nome:,
+    //     nota:
+    // }
+let nomeDisciplina = []
+let notaDisciplina = []
 
-butao.addEventListener('click', () => {
-    let mostraDisciplina = document.getElementsByTagName('h1')[0]
-    let nomeDisc = document.querySelector('#disc-nome')
-    let notaDisc = document.querySelector('#disc-nota')
-    let disciplinas = document.querySelector('#txt-numero')
-    let numeroDisciplinas = Number(disciplinas.value)
-    let nomeDisciplina = []
-    let notaDisciplina = []
-    if (numeroDisciplinas < 2) {
-        alert('O numero de disciplinas tem de ser maior ou igual a 2')
-    } else {
-        butao.style.display = 'none'
-        formEntrada.style.display = 'none'
-        formRegistar.style.display = 'block'
-        butaoRegistar.style.display = 'block'
+let cont = 0
+ocultar(formRegistar, butaoRegistar)
+ocultar(paragrafo)
+
+function iniciarAnalise() {
+    if (validarNumDisciplinas()) {
+        ocultar(butao, formEntrada)
+
+        mostrar(formRegistar, butaoRegistar)
+
         divPrincipal.classList.add('estiloregisto')
+        atualizarTituloDisciplina()
 
-        // const dadosDisciplina = {
-        //     nome: nomeDisciplina.value,
-        //     nota: Number(notaDisciplina.value)
-        // }
-
-        let cont = 1
-        mostraDisciplina.innerHTML = `Disciplina ${cont}`
-        butaoRegistar.addEventListener('click', () => {
-                if (nomeDisc.value.length === 0 || notaDisc.value.length === 0) {
-                    alert('Dado invalido!')
-                        // formRegistar.innerHTML += ''
-                        // formRegistar.innerHTML += "<p>O campo NOME DA DISCIPLINA não pode estar vazio, e a nota tem estar entre 0 a 20</p>"
-                } else {
-                    alert('Dado Inserido!')
-                    if (cont < numeroDisciplinas) {
-                        cont++
-                        mostraDisciplina.innerHTML = `Disciplina ${cont}`
-                        nomeDisciplina.push(nomeDisc.value)
-                        notaDisciplina.push(Number(notaDisc.value))
-                        nomeDisc.value = ''
-                        nomeDisc.focus()
-                        notaDisc.value = 0
-                    }
-                }
-
-            })
-            // for (let cont = 1; cont <= numeroDisciplinas; cont++) 
-            // }
-
-        alert('Tudo Ok!')
     }
-    solucao.innerHTML = `Nomes ${nomeDisciplina}<br><br>`
-    solucao.innerHTML += `Notas ${notaDisciplina}`
+}
+
+function atualizarTituloDisciplina() {
+    mostraDisciplina.innerHTML = `Disciplina ${Math.min(cont+1,disciplinas.value)}`
+}
+
+function validarNumDisciplinas() {
+    if (+disciplinas.value < 2) {
+        alert('O numero de disciplinas tem de ser maior ou igual a 2')
+        return false
+    } else {
+        return true
+    }
+
+}
+
+function mostrar(...elem) {
+    elem.forEach(el => el.style.display = 'block')
+}
+
+function ocultar(...elem) {
+    elem.forEach(el => el.style.display = 'none')
+}
+
+function registar() {
+    if (nomeDisc.value.length === 0 || notaDisc.value.length === 0 || +notaDisc.value < 0 || +notaDisc.value > 20) {
+        // alert('Dado invalido!')
+        mostrar(paragrafo)
+
+    } else {
+        ocultar(paragrafo)
+        cont++
+        if (cont <= +disciplinas.value) {
+
+            atualizarTituloDisciplina()
+            nomeDisciplina.push(nomeDisc.value)
+            notaDisciplina.push(+notaDisc.value)
+            nomeDisc.value = ''
+            nomeDisc.focus()
+            notaDisc.value = 0
+            if (cont == +disciplinas.value) console.log('Terminou!')
+        }
+    }
+    solucao.innerHTML = `Nomes ${ nomeDisciplina}<br><br>`
+    solucao.innerHTML += `Notas ${ notaDisciplina}`
     solucao.style.visibility = "visible";
-})
+}
+butaoRegistar.addEventListener('click', registar)
+butao.addEventListener('click', iniciarAnalise)
