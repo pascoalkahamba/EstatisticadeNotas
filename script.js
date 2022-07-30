5d3fba8ff8b66ca33b0751683052fea235a90295
pascoalkahamba <pascoalkahamba25@gmail.com>
2022-07-30 13:12:08 +0100

    ultimas fucionalidades

--- script.js
+++ script.js
@@ -1,248 +1,259 @@
-const $ = document.querySelector.bind(document)
-let solucao = $('.solucao')
-let butao = $('#butao')
-let formEntrada = $('.form1')
-let formRegistar = $('#form2')
-let butaoRegistar = document.getElementById('butaoRegistar')
-let divPrincipal = $('.problema')
-let mostraDisciplina = document.getElementsByTagName('h1')[0]
-let nomeDisc = $('#disc-nome')
-let notaDisc = $('#disc-nota')
-let disciplinas = $('#txt-numero')
-let paragrafo = $('.paragrafo')
-let mostrarResultados = $('.resultadoAnalise')
-let escolhaPesquisa = document.getElementsByName('txt-dados')
-let campoPesquisa = $('#nomeDisciplina')
-let nomeDisciplina = []
-let notaDisciplina = []
-let tabela = $('.tabela')
-let mostraDadosPesquisa = $('.buscaDados')
-let butaoPesquisaNomeDisc = $('#butaoPesquisa')
-let butaoPesquisaNotaDisc = $('#butaoPesquisaNota')
-let valorProcurado = $('#nomeDisciplina')
-let notaNegativa = $('.resultadoAnalise div')
-let rodape = document.getElementsByTagName('footer')[0]
-let pessimaDisciplina
-let melhorDisciplina
-let notaBaixa
-let cont = 0
+const $ = document.querySelector.bind(document);
+let solucao = $(".solucao");
+let butao = $("#butao");
+let formEntrada = $(".form1");
+let formRegistar = $("#form2");
+let butaoRegistar = document.getElementById("butaoRegistar");
+let divPrincipal = $(".problema");
+let mostraDisciplina = document.getElementsByTagName("h1")[0];
+let nomeDisc = $("#disc-nome");
+let notaDisc = $("#disc-nota");
+let disciplinas = $("#txt-numero");
+let paragrafo = $(".paragrafo");
+let mostrarResultados = $(".resultadoAnalise");
+let escolhaPesquisa = document.getElementsByName("txt-dados");
+let campoPesquisa = $("#nomeDisciplina");
+let nomeDisciplina = [];
+let notaDisciplina = [];
+let tabela = $(".tabela");
+let mostraDadosPesquisa = $(".buscaDados");
+let butaoPesquisaNomeDisc = $("#butaoPesquisa");
+let butaoPesquisaNotaDisc = $("#butaoPesquisaNota");
+let valorProcurado = $("#nomeDisciplina");
+let notaNegativa = $(".resultadoAnalise div");
+let rodape = document.getElementsByTagName("footer")[0];
+let pessimaDisciplina;
+let melhorDisciplina;
+let notaBaixa;
+let cont = 0;
+let campoVermelho = $(".tabela td");
 
 function escolhaBusca() {
-    escolhaPesquisa.forEach(elem => {
-        elem.onchange = function() {
-            if (escolhaPesquisa[1].checked) {
-                ocultar(butaoPesquisaNomeDisc)
-                mostrar(butaoPesquisaNotaDisc)
-                addAtributoButaoNota()
-            } else {
-                ocultar(butaoPesquisaNotaDisc)
-                margemButao()
-                mostrar(butaoPesquisaNomeDisc)
-                addAtributoButaoNome()
-            }
-        }
-    })
+  escolhaPesquisa.forEach((elem) => {
+    elem.onchange = function () {
+      if (escolhaPesquisa[1].checked) {
+        ocultar(butaoPesquisaNomeDisc);
+        mostrar(butaoPesquisaNotaDisc);
+        addAtributoButaoNota();
+      } else {
+        ocultar(butaoPesquisaNotaDisc);
+        margemButao();
+        mostrar(butaoPesquisaNomeDisc);
+        addAtributoButaoNome();
+      }
+    };
+  });
 }
 
 function addAtributoButaoNota() {
-    campoPesquisa.setAttribute('type', 'number')
-    campoPesquisa.setAttribute('value', '')
-    campoPesquisa.setAttribute('placeholder', 'Nota')
+  campoPesquisa.setAttribute("type", "number");
+  campoPesquisa.setAttribute("value", "");
+  campoPesquisa.setAttribute("placeholder", "Nota");
 }
 
 function addAtributoButaoNome() {
-    campoPesquisa.setAttribute('type', 'text')
-    campoPesquisa.setAttribute('placeholder', 'Nome da Disciplina')
+  campoPesquisa.setAttribute("type", "text");
+  campoPesquisa.setAttribute("placeholder", "Nome da Disciplina");
 }
 
 function margemButao() {
-    butaoPesquisaNomeDisc.style.marginTop = '-38px'
-    butaoPesquisaNomeDisc.style.marginLeft = '225px'
-
+  butaoPesquisaNomeDisc.style.marginTop = "-38px";
+  butaoPesquisaNomeDisc.style.marginLeft = "225px";
 }
 
-ocultar(formRegistar, butaoRegistar, solucao, paragrafo, mostraDadosPesquisa, butaoPesquisaNotaDisc)
+ocultar(
+  formRegistar,
+  butaoRegistar,
+  solucao,
+  paragrafo,
+  mostraDadosPesquisa,
+  butaoPesquisaNotaDisc
+);
 
 function iniciarAnalise() {
-    if (validarNumDisciplinas()) {
-        ocultar(butao, formEntrada)
+  if (validarNumDisciplinas()) {
+    ocultar(butao, formEntrada);
 
-        mostrar(formRegistar, butaoRegistar)
+    mostrar(formRegistar, butaoRegistar);
 
-        divPrincipal.classList.add('estiloregisto')
-        atualizarTituloDisciplina()
-
-    }
+    divPrincipal.classList.add("estiloregisto");
+    atualizarTituloDisciplina();
+  }
 }
 
 function buscaValoresArray(array, valor) {
-    if (array.indexOf(valor) != -1) {
-        array.forEach((elem, index) => {
-            if (escolhaPesquisa[0].checked) {
-                if (elem === valor) {
-                    mostraDadosPesquisa.innerHTML = ''
-                    mostraDadosPesquisa.innerHTML = '<h1>Resultado/s da Busca</h1>'
-                    mostraDadosPesquisa.innerHTML += `<p>Em ${elem} tiveste ${notaDisciplina[index]} valores</p>`
-                }
-            } else {
-                if (elem === valor) {
-                    mostraDadosPesquisa.innerHTML = ''
-                    mostraDadosPesquisa.innerHTML = '<h1>Resultado/s da Busca</h1>'
-                }
-            }
-
-        })
-        mostrar(mostraDadosPesquisa)
-    } else {
-        alert('Este valor não existe na sua Pauta!')
-
-    }
-
+  if (array.indexOf(valor) != -1) {
+    array.forEach((elem, index) => {
+      if (escolhaPesquisa[0].checked) {
+        if (elem === valor) {
+          mostraDadosPesquisa.innerHTML = "";
+          mostraDadosPesquisa.innerHTML = "<h1>Resultado/s da Busca</h1>";
+          mostraDadosPesquisa.innerHTML += `<p>Em ${elem} tiveste ${notaDisciplina[index]} valores</p>`;
+        }
+      } else {
+        if (elem === valor) {
+          mostraDadosPesquisa.innerHTML = "";
+          mostraDadosPesquisa.innerHTML = "<h1>Resultado/s da Busca</h1>";
+        }
+      }
+    });
+    mostrar(mostraDadosPesquisa);
+  } else {
+    alert("Este valor não existe na sua Pauta!");
+  }
 }
 
 function estilodoBodyEfooter() {
-    window.document.body.style.height = '100%'
-    rodape.style.marginTop = '20px'
+  window.document.body.style.height = "100%";
+  rodape.style.marginTop = "20px";
 }
 
 function chamafuncaoNome() {
-    buscaValoresArray(nomeDisciplina, valorProcurado.value)
+  buscaValoresArray(nomeDisciplina, valorProcurado.value);
 }
 
 function chamafuncaoNota() {
-    buscaValoresArray(notaDisciplina, +valorProcurado.value)
-    notaDisciplina.filter((item, index) => {
-        if (item == +valorProcurado.value) {
-            mostraDadosPesquisa.innerHTML += `<p>Tiveste ${item} valores em ${nomeDisciplina[index]}</p>`
-        }
-    })
+  buscaValoresArray(notaDisciplina, +valorProcurado.value);
+  notaDisciplina.filter((item, index) => {
+    if (item == +valorProcurado.value) {
+      mostraDadosPesquisa.innerHTML += `<p>Tiveste ${item} valores em ${nomeDisciplina[index]}</p>`;
+    }
+  });
 }
 
 function atualizarTituloDisciplina() {
-    mostraDisciplina.innerHTML = `Disciplina ${Math.min(cont+1,disciplinas.value)}`
+  mostraDisciplina.innerHTML = `Disciplina ${Math.min(
+    cont + 1,
+    disciplinas.value
+  )}`;
 }
 
 function validarNumDisciplinas() {
-    if (+disciplinas.value < 2) {
-        alert('O numero de disciplinas tem de ser maior ou igual a 2')
-        return false
-    } else {
-        return true
-    }
-
+  if (+disciplinas.value < 2) {
+    alert("O numero de disciplinas tem de ser maior ou igual a 2");
+    return false;
+  } else {
+    return true;
+  }
 }
 
 function mostrar(...elem) {
-    elem.forEach(el => el.style.display = 'block')
+  elem.forEach((el) => (el.style.display = "block"));
 }
 
 function ocultar(...elem) {
-    elem.forEach(el => el.style.display = 'none')
+  elem.forEach((el) => (el.style.display = "none"));
 }
 
 function validarDisciplina(disc, elem) {
-    if (disc.indexOf(elem) == -1) {
-        return true
-    } else {
-        alert('Desculpe esta disciplina já foi adicionada!')
-        cont--
-        nomeDisc.value = ''
-        nomeDisc.focus()
-        return false
-    }
+  if (disc.indexOf(elem) == -1) {
+    return true;
+  } else {
+    alert("Desculpe esta disciplina já foi adicionada!");
+    cont--;
+    nomeDisc.value = "";
+    nomeDisc.focus();
+    return false;
+  }
 }
-
 
 // function estiloNotaAbaixo() {
 //     notaNegativa.setAttribute('class', 'notaBaixa')
 // }
 
 function notasBaixas() {
-    +notaDisciplina.forEach(array => {
-        if (array < 10) {
-            console.log('nota baixa')
-                // estiloNotaAbaixo()
-        } else {
-            return false
-        }
-    })
-
+  +notaDisciplina.forEach((array) => {
+    if (array < 10) {
+      console.log("nota baixa");
+      // estiloNotaAbaixo()
+    } else {
+      return false;
+    }
+  });
 }
 
 function maiorNotaEmenorNota() {
-    notaAlta = notaDisciplina[0]
-    notaBaixa = notaDisciplina[0]
-    let soma = 0
-    let tamanhoArray = notaDisciplina.length
-    notaDisciplina.forEach((elem, index) => {
-        soma += +elem
-        if (elem > notaAlta) {
-            notaAlta = elem
-            melhorDisciplina = nomeDisciplina[index]
-        }
-        if (elem < notaBaixa) {
-            notaBaixa = elem
-            pessimaDisciplina = nomeDisciplina[index]
-        }
-    })
-    media = +(soma / tamanhoArray)
-
+  notaAlta = notaDisciplina[0];
+  notaBaixa = notaDisciplina[0];
+  let soma = 0;
+  let tamanhoArray = notaDisciplina.length;
+  notaDisciplina.forEach((elem, index) => {
+    soma += +elem;
+    if (elem > notaAlta) {
+      notaAlta = elem;
+      melhorDisciplina = nomeDisciplina[index];
+    }
+    if (elem < notaBaixa) {
+      notaBaixa = elem;
+      pessimaDisciplina = nomeDisciplina[index];
+    }
+  });
+  media = +(soma / tamanhoArray);
+}
+function verificarNota() {
+  if (notaAlta < 10 || notaBaixa < 10 || media < 10) {
+    campoVermelho.style.backgroundColor = "red";
+  }
 }
 
 function ultimoRegisto() {
-    if (cont == +disciplinas.value) {
-        alert("Ultimo registo")
-        mostrar(solucao)
-        mostraDisciplina.style.marginTop = '-10px'
-        mostraDisciplina.innerHTML = "Resultados da Análise"
-        ocultar(formRegistar, butaoRegistar)
-        mostrarDados()
-
-    }
+  if (cont == +disciplinas.value) {
+    alert("Ultimo registo");
+    mostrar(solucao);
+    mostraDisciplina.style.marginTop = "-10px";
+    mostraDisciplina.innerHTML = "Resultados da Análise";
+    ocultar(formRegistar, butaoRegistar);
+    mostrarDados();
+  }
 }
 
 function mostrarDados() {
-    let cont = 0
-    for (let conta = 1; conta <= disciplinas.value; conta++) {
-        mostrarResultados.innerHTML += `<div><span>${nomeDisciplina[cont]}:</span><span>${notaDisciplina[cont]}V</span></div>`
-        cont++
-    }
-    notasBaixas()
-    tabela.innerHTML +=
-        `<tr>
+  let cont = 0;
+  for (let conta = 1; conta <= disciplinas.value; conta++) {
+    const ehNeg = notaDisciplina[cont] < 10 ? "notaBaixa" : "";
+
+    mostrarResultados.innerHTML += `<div class='${ehNeg}' ><span>${nomeDisciplina[cont]}:</span><span>${notaDisciplina[cont]}V</span></div>`;
+    cont++;
+  }
+  notasBaixas();
+  // verificarNota()
+  tabela.innerHTML += `<tr>
             <td>${notaAlta} (${melhorDisciplina})</td>
             <td>${notaBaixa} (${pessimaDisciplina})</td>
             <td>${media}</td>
-        </tr>`
-    escolhaBusca()
-    estilodoBodyEfooter()
+        </tr>`;
+  escolhaBusca();
+  estilodoBodyEfooter();
 }
 
 function registar() {
-    if (nomeDisc.value.length === 0 || notaDisc.value.length === 0 || +notaDisc.value < 0 || +notaDisc.value > 20) {
-        mostrar(paragrafo)
+  if (
+    nomeDisc.value.length === 0 ||
+    notaDisc.value.length === 0 ||
+    +notaDisc.value < 0 ||
+    +notaDisc.value > 20
+  ) {
+    mostrar(paragrafo);
+  } else {
+    ocultar(paragrafo);
+    cont++;
+    if (cont <= +disciplinas.value) {
+      if (validarDisciplina(nomeDisciplina, nomeDisc.value)) {
+        atualizarTituloDisciplina();
+        nomeDisciplina.push(nomeDisc.value);
+        notaDisciplina.push(+notaDisc.value);
 
-    } else {
-        ocultar(paragrafo)
-        cont++
-        if (cont <= +disciplinas.value) {
-
-            if (validarDisciplina(nomeDisciplina, nomeDisc.value)) {
-                atualizarTituloDisciplina()
-                nomeDisciplina.push(nomeDisc.value)
-                notaDisciplina.push(+notaDisc.value)
-
-                maiorNotaEmenorNota()
-                nomeDisc.value = ''
-                nomeDisc.focus()
-                notaDisc.value = 0
-            }
-            ultimoRegisto()
-        }
+        maiorNotaEmenorNota();
+        nomeDisc.value = "";
+        nomeDisc.focus();
+        notaDisc.value = 0;
+      }
+      ultimoRegisto();
     }
+  }
 }
 
-butaoRegistar.addEventListener('click', registar)
-butao.addEventListener('click', iniciarAnalise)
-butaoPesquisaNomeDisc.addEventListener('click', chamafuncaoNome)
-butaoPesquisaNotaDisc.addEventListener('click', chamafuncaoNota)
+butaoRegistar.addEventListener("click", registar);
+butao.addEventListener("click", iniciarAnalise);
+butaoPesquisaNomeDisc.addEventListener("click", chamafuncaoNome);
+butaoPesquisaNotaDisc.addEventListener("click", chamafuncaoNota);
